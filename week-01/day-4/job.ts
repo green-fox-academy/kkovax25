1957945db0f85e2d01521c5521d8ee96ab348591
Kovács Krisztián <kkovax94@gmail.com>
2019-09-12 16:19:22 +0200

    Finished the calculation part

--- week-01/day-4/job.ts
+++ week-01/day-4/job.ts
@@ -22,3 +22,11 @@ console.log(numberOne - numberTwo);
 console.log(numberOne * numberTwo);
 console.log(numberOne / numberTwo);
 console.log(numberOne % numberTwo);
+
+let totalWeek: number = 6 * 5 * 17;
+console.log(totalWeek);
+let totalHour: number = 52 * 17
+console.log(totalHour);
+
+let percentageTotal: number = (totalWeek / totalHour) * 100;
+console.log(Math.round(percentageTotal));
