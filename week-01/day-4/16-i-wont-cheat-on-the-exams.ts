3632dd195db658e559a16795729d9707404edd04
Kovács Krisztián <kkovax94@gmail.com>
2019-09-13 09:53:50 +0200

    Finished the cheat exercise

---
+++ week-01/day-4/16-i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let a: number = 0;
+let cheatLine: string = "I won't cheat on the exam!"
+while (a <= 100) {
+    console.log(cheatLine);
+    a++;
+}
