d3d182eed74f0506fb2fd2b572f8a97c0f5b1a1f
Kovács Krisztián <kkovax94@gmail.com>
2019-09-13 10:35:32 +0200

    Completed the print-even exercise

---
+++ week-01/day-4/17-print-even.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+// let myNumber: number = 0;
+
+for (let i: number = 1; i <=500; i++){
+    if (i % 2 === 0){
+        console.log(i);
+    }
+}
