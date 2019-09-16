3e262155b466b92d6bfff1cbbff2e9c8dd0e6734
Kovács Krisztián <kkovax94@gmail.com>
2019-09-16 19:27:37 +0200

    finished the summ

--- week-02/day-01/11-summ-all.ts
+++ week-02/day-01/11-summ-all.ts
@@ -1,7 +1,10 @@
 // -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // -  Log the sum of the elements in `ai` to the console
 
-let ai: number[] = [3,4,5,6,7,1];
-let sum =  ai.reduce((result,number)=> result+number);
+let ai: number[] = [3,4,5,6,7];
+let sum = 0;
+ ai.forEach(function(element){
+     sum += element;
+ });
 
 console.log(sum)
